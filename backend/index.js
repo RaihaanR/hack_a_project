76353b23d5dd37c9ef9477d752c9d2c2e2c3dfac
Devesh Erda <45883732+devesh-erda@users.noreply.github.com>
2020-02-29 10:16:45 +0000

    Added events endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 var express = require('express')
 var app = express()
 
+var fs = require('fs')
+
 app.get('/', function (req, res) {
-  res.send('')
+  res.send('');
+})
+
+app.get('/events', function (req, res) {
+  fs.readFile('./data/data.json'), function (err, data) {
+    if (err) {
+      throw err;
+    }
+  }
+  console.log(data.toString());
 })
 
 app.listen(3000)
