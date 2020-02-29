c2157707d93715e1ee7bd50dea95f45443810b3f
lukyxu <luky.xu@gmail.com>
2020-02-29 12:06:01 +0000

    Changed to res.json()

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,13 @@ app.get('/', function (req, res) {
 
 app.get('/events', function (req, res) {
   var data = fs.readFileSync('./data/data.json');
-  res.send(JSON.parse(data));
+  res.json(JSON.parse(data)["events"]);
 })
 
 app.get('/events/:id', function (req, res) {
   var id = req.params.id;
   var data = JSON.parse(fs.readFileSync('./data/data.json'))['events'];
-
-  res.send(data[id]);
+  res.json(data[id]);
 })
 
 app.post('/events', function (req, res) {
@@ -27,7 +26,6 @@ app.post('/events', function (req, res) {
   if (newEvent["name"] == null || newEvent["organiser"] == null || newEvent["location"] == null || newEvent["time"] == null || newEvent["image"] == null) {
     return res.status(400).send({ message: "Invalid event format"})
   }
-  console.log(events)
   events.push(newEvent)
   newEvent["id"] = events.length
   newEvent["post_time"] = new Date()
@@ -35,9 +33,34 @@ app.post('/events', function (req, res) {
 
   fs.writeFile('./data/data.json', JSON.stringify({events: events}), 'utf8', function(err){
     if (err) {
-      return res.status(500).send({ message: "Internal server format"})
+      return res.status(500).json({ message: "Internal server format"})
     }
-    return res.status(200).send(events)
+    return res.status(200).json(events)
+  })
+})
+
+app.get('/users', function (req, res) {
+  var data = fs.readFileSync('./data/users.json');
+  res.json(JSON.parse(data)["users"]);
+})
+
+app.post('/users', function (req, res) {
+  var data = fs.readFileSync('./data/users.json');
+  var users = JSON.parse(data)["users"];
+  var newUser = req.body;
+  console.log(newUser)
+  if (newUser["username"] == null) {
+    return res.status(400).json({ message: "Invalid event format"})
+  }
+  users.push(newUser)
+  newUser["id"] = users.length
+  newUser["events"] = []
+
+  fs.writeFile('./data/users.json', JSON.stringify({users: users}), 'utf8', function(err){
+    if (err) {
+      return res.status(500).json({ message: "Internal server format"})
+    }
+    return res.status(200).json(users)
   })
 })
 
