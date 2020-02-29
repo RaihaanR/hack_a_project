fe5b752e630bc48297f8a6706f2328c7cd083b69
Devesh Erda <45883732+devesh-erda@users.noreply.github.com>
2020-02-29 10:59:50 +0000

    Returning JSON object instead of string when sending all events

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
-var express = require('express')
-var app = express()
-
-var fs = require('fs')
+var express = require('express');
+var fs = require('fs');
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('');
@@ -9,13 +8,14 @@ app.get('/', function (req, res) {
 
 app.get('/events', function (req, res) {
   var data = fs.readFileSync('./data/data.json');
-  res.send(data.toString());
+  res.send(JSON.parse(data));
 })
 
 app.get('/events/:id', function (req, res) {
   var id = req.params.id;
-  var data = JSON.parse(fs.readFileSync('./data/data.json'))['events'][id];
-  res.send(data);
+  var data = JSON.parse(fs.readFileSync('./data/data.json'))['events'];
+
+  res.send(data[id]);
 })
 
-app.listen(3000)
+app.listen(3000);
