8fce5b639355bcb240fc37fc6db7b567bb90363a
lukyxu <luky.xu@gmail.com>
2020-02-29 11:12:08 +0000

    added post events

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ var app = express()
 
 var fs = require('fs')
 
+app.use(express.json());
+
 app.get('/', function (req, res) {
   res.send('');
 })
@@ -10,6 +12,26 @@ app.get('/', function (req, res) {
 app.get('/events', function (req, res) {
   var data = fs.readFileSync('./data/data.json');
   console.log(data.toString());
+})
+
+app.post('/events', function (req, res) {
+  var data = fs.readFileSync('./data/data.json');
+  var events = JSON.parse(data)["events"];
+  var newEvent = req.body;
+  if (newEvent["name"] == null || newEvent["organizer"] == null || newEvent["location"] == null) {
+    return res.status(400).send({ message: "Invalid event format"})
+  }
+  console.log(events)
+  events.push(newEvent)
+  newEvent["id"] = events.length
+  newEvent["time"] = new Date()
+
+  fs.writeFile('./data/data.json', JSON.stringify({events: events}), 'utf8', function(err){
+    if (err) {
+      return res.status(500).send({ message: "Internal server format"})
+    }
+    return res.status(200).send(events)
+  })
 })
 
 app.listen(3000)
