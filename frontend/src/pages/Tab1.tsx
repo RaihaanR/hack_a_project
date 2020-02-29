77fff59e412b230c0f4b4cbff8e7b43786696bf3
Ziwen Yuan <yuanziwen1014@gmail.com>
2020-02-29 16:41:53 +0000

    tab1 organization page

--- frontend/src/pages/Tab1.tsx
+++ frontend/src/pages/Tab1.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonFabList,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonItem,
+  IonButton,
+  IonLabel,
+  IonSegment,
+  IonSegmentButton,
+  IonModal,
+  IonImg,
+  IonInput
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab1.css";
 
 const Tab1: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const form = <IonInput></IonInput>;
+
   return (
     <IonPage>
       <IonHeader>
@@ -20,10 +41,113 @@ const Tab1: React.FC = () => {
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Hi, Organizers!</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {/* <ExploreContainer name="Tab 1 page" /> */}
+        <IonFab vertical="center" horizontal="end" slot="fixed">
+          <IonFabButton>
+            <IonIcon name="add" />
+          </IonFabButton>
+        </IonFab>
+
+        <IonCard onClick={() => setShowModal(true)}>
+          <IonCardHeader>
+            <IonCardSubtitle></IonCardSubtitle>
+            <IonCardTitle>Add Your New Event Now!</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            <IonIcon />
+            <br />
+            <IonIcon />
+          </IonCardContent>
+        </IonCard>
+
+        <IonModal isOpen={showModal}>
+          <IonCard>
+            <h1 className="ion-text-center">New Event</h1>
+          </IonCard>
+          <IonCard>
+            <IonItem>
+              <IonLabel>Event Name:</IonLabel>
+              <IonInput placeholder="Enter Name"></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Event Date:</IonLabel>
+              <IonInput placeholder="when is it"></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Event Location: </IonLabel>
+              <IonInput placeholder="where is this event"></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Event Description: </IonLabel>
+              <IonInput placeholder="Describe Your event"></IonInput>
+            </IonItem>
+          </IonCard>
+          <IonButton onClick={() => setShowModal(false)}>Submit</IonButton>
+          <IonButton onClick={() => setShowModal(false)}>Cancle</IonButton>
+          {/* <IonButton onClick={() => setShowModal(false)}>Submit</IonButton> */}
+          {/* <IonCardContent>
+            <IonSegment
+              value="cancel"
+              onIonChange={e => console.log("Segment selected", e.detail.value)}
+            >
+              <IonSegmentButton
+                value="submit"
+                onClick={() => setShowModal(false)}
+              >
+                <IonLabel>Submit</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton
+                value="cancel"
+                onClick={() => setShowModal(false)}
+              >
+                <IonLabel>Cancel</IonLabel>
+              </IonSegmentButton>
+            </IonSegment>
+          </IonCardContent> */}
+        </IonModal>
+        {/* <IonButton onClick={() => setShowModal(true)}>Show Modal</IonButton> */}
+
+        {/* <IonSegment
+          value="no"
+          onIonChange={e => console.log("Segment selected", e.detail.value)}
+        >
+          <IonSegmentButton value="going">
+            <IonLabel>Going</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="no">
+            <IonLabel>No</IonLabel>
+          </IonSegmentButton>
+        </IonSegment> */}
+
+        {/* <IonFab vertical="center" horizontal="end" slot="fixed">
+          <IonFabButton>
+            <IonIcon name="share" />
+          </IonFabButton>
+          <IonFabList side="top">
+            <IonFabButton>
+              <IonIcon name="logo-vimeo" />
+            </IonFabButton>
+          </IonFabList>
+          <IonFabList side="bottom">
+            <IonFabButton>
+              <IonIcon name="logo-facebook" />
+            </IonFabButton>
+          </IonFabList>
+          <IonFabList side="start">
+            <IonFabButton>
+              <IonIcon name="logo-instagram" />
+            </IonFabButton>
+          </IonFabList>
+          <IonFabList side="end">
+            <IonFabButton>
+              <IonIcon name="logo-twitter" />
+            </IonFabButton>
+          </IonFabList>
+        </IonFab> */}
       </IonContent>
     </IonPage>
   );
--- frontend/src/pages/Tab3.tsx
+++ frontend/src/pages/Tab3.tsx
@@ -1,45 +1,126 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Profile from "./Profile";
 import "./Tab3.css";
 
 import {
+  IonSearchbar,
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
   IonToolbar,
   IonGrid,
-  IonRow
+  IonRow,
+  IonCard,
+  IonImg,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonIcon
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab3.css";
 import "./Profile.css";
+import { location, time } from "ionicons/icons";
+
+type Event = {
+  id: number;
+  image: string;
+  organiser: string;
+  name: string;
+  location: string;
+  description: string;
+  time: string;
+};
 
 const Tab3: React.FC = () => {
+  var nullEvent: Event = {
+    id: -1,
+    image: "",
+    organiser: "null",
+    name: "null",
+    location: "null",
+    description: "null",
+    time: "null"
+  };
+
+  const [modalEvent, setModalEvent] = useState(nullEvent);
+  const [showModal, setShowModal] = useState(false);
+  const [events, setEvent] = useState([nullEvent]);
+
+  function openEvent(e: Event) {
+    setModalEvent(e);
+    setShowModal(true);
+  }
+
+  // useEffect(() => {
+  //   fetchUserEvents();
+  //   console.log(id);
+  // }, []);
+
+  const [item, setItem] = useState([]);
+  let username = "DoCSoc";
+  // const fetchUserEvents = async () => {
+  //   const data = await fetch(`https://5498e4a8.ngrok.io/events/users/get?username=${username}/events`);
+  //   //`https://fortnite-api.theapinetwork.com/item/get?id=${match.params.id}`
+
+  //   const events= await data.json();
+  //   console.log(events);
+  // };
+
+  useEffect(() => {
+    fetch("https://5498e4a8.ngrok.io/users/DoCSoc/events")
+      .then(res => res.json())
+      .then(result => {
+        const eventList: Event[] = [];
+        (result as Event[]).forEach(element => {
+          eventList.push(element);
+          console.log(events);
+        });
+        setEvent(eventList);
+      });
+  }, [events.length]);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Your Profile</IonTitle>
+          <IonTitle>DoCSoc</IonTitle>
         </IonToolbar>
       </IonHeader>
 
-      <IonContent
-        scrollEvents={true}
-        onIonScrollStart={() => {}}
-        onIonScroll={() => {}}
-        onIonScrollEnd={() => {}}
-      >
-        <h1 className="ion-text-center">Student Name</h1>
+      <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Student Name</IonTitle>
+          </IonToolbar>
+        </IonHeader>
 
-        <IonGrid>
+        <h1 className="ion-text-center">Going</h1>
+
+        {/* <IonGrid>
           <IonRow className="ion-margin-start">
             Email: firstname.lastname@uni.ac.uk
           </IonRow>
-          <IonRow className="ion-margin-start">
-            Interests: Facebook, Machine Learning, Computer Science
-          </IonRow>
-        </IonGrid>
+          <IonRow className="ion-margin-start">Interests: Facebook</IonRow>
+        </IonGrid> */}
+        <IonSearchbar showCancelButton="always"></IonSearchbar>
+
+        {events.map(event => (
+          <IonCard>
+            <IonImg src={event.image} />
+            <IonCardHeader>
+              <IonCardSubtitle>By {event.organiser}</IonCardSubtitle>
+              <IonCardTitle>{event.name}</IonCardTitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              <IonIcon icon={location} /> {event.location} <br />
+              <IonIcon icon={time} /> {event.time}
+            </IonCardContent>
+          </IonCard>
+        ))}
       </IonContent>
     </IonPage>
   );
